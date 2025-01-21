c2dd3efe114b8c45e6a883d2054670940ea4290f
Mara <maraolaru30@gmail.com>
2025-01-21 12:15:55 +0200

    added animation for all droplets and transition to the next scene

--- Assets/Scripts/DropletColliderInteraction.cs
+++ Assets/Scripts/DropletColliderInteraction.cs
@@ -3,68 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class DropletColliderInteraction : MonoBehaviour
 {
 	public GameObject currentContactObject;
 	public GameObject nextContactObject;
 
-	public AudioSource contactAudio;
+	public AudioSource waterSound;
 
 	public ParticleSystem rainParticleSystem;
 	public ParticleSystem onContactParticleSystem;
 
-	//public static float rateOverTimeIncrease = 100f;
+	public static float rateOverTimeIncrease = 100f;
 	public static int timesTouchedCollider = 0;
+
+	private float dropStartTime;
+
+	/*void Update()
+    {
+        // if the user did not touch the droplet, after a certain period, make it dissapear and go to the next droplet
+        if (currentContactObject.activeSelf)
+        {
+			// get the current time when the object activates
+			dropStartTime = Time.time;
+			Debug.Log("current frame:" + Time.time);
+            if (Time.time - dropStartTime > 20f)
+            {
+				currentContactObject.SetActive(false);
+				nextContactObject.SetActive(true);
+			}
+
+		}
+    }*/
 
 	void OnTriggerEnter(Collider other)
 	{
 
 		onContactParticleSystem.Play();
-		contactAudio.Play(); //cand voi avea audio ul
+		waterSound.Play(); // replace it with bits from the song
 
 		currentContactObject.SetActive(false);
 		nextContactObject.SetActive(true);
 
+        if (timesTouchedCollider < 8)
+        {
+			rateOverTimeIncrease += 500f;
+			var emission = rainParticleSystem.emission;
+			emission.rateOverTime = rateOverTimeIncrease;
+        }
+        else
+        {
+			// hide the last object and go to the next scene
+			currentContactObject.SetActive(false);
+			SceneManager.LoadScene("Rain");
+		}
+
 		timesTouchedCollider++;
-
-		if (timesTouchedCollider == 1)
-		{
-			var emission = rainParticleSystem.emission;
-			emission.rateOverTime = 600f;
-		}
-
-		if (timesTouchedCollider == 2)
-		{
-			var emission = rainParticleSystem.emission;
-			emission.rateOverTime = 800f;
-		}
-
-		if (timesTouchedCollider == 3)
-		{
-			var emission = rainParticleSystem.emission;
-			emission.rateOverTime = 1000f;
-		}
-
-
-
-
-
-		//currentContactObject.SetActive(false);
-		//nextContactObject.SetActive(true);
-
-		//var emission = rainParticleSystem.emission;
-		//emission.rateOverTime = rateOverTimeIncrease;
-
-		//Debug.Log("drop touched");
-
-
-		//rateOverTimeIncrease += 100; //with every touch of a droplet, increase the droplets
-
-		//if (timesTouchedCollider == 4)
-		//{
-		//	// scene transition
-		//	SceneManager.LoadScene("<name of the scene>");
-		//}
 	}
 }
--- Assets/Scripts/StartRain.cs
+++ Assets/Scripts/StartRain.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class StartRain : MonoBehaviour
 {
     public ParticleSystem rainParticleSystem;
+    public GameObject firstDrop;
+    public int waitTime=105;
 
     void playRain()
     {
         rainParticleSystem.Play();
+
+        // wait a couple of seconds, start showing the first drop
+        StartCoroutine(Waiter());
+        firstDrop.SetActive(true);
+    }
+
+    IEnumerator Waiter()
+    {
+        yield return new WaitForSecondsRealtime(waitTime);
+
     }
 
 
